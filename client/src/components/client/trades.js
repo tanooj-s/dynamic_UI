b0b6b71260170761ef60aadfa30eb978535a7a00
Tanooj Shah <tanooj@Tanoojs-MacBook-Air.local>
2020-03-19 17:39:14 +0530

    added filter for trades table

--- client/src/components/client/trades.js
+++ client/src/components/client/trades.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 import { Table } from 'reactstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { textFilter, selectFilter, numberFilter,Comparator  } from 'react-bootstrap-table2-filter';
 
 function Trades(props) {
 
@@ -20,7 +21,8 @@ function Trades(props) {
             },{
               dataField: 'OwnsSymbol',
               text: 'Symbol',
-              sort: true
+              sort: true,
+              filter: textFilter({ caseSensitive: false }),
             },{
               dataField: 'ISIN',
               text: 'ISIN',
@@ -28,51 +30,53 @@ function Trades(props) {
             },{
               dataField: 'Volume',
               text: 'Volume',
-              sort: true
+              sort: true,
+              filter: numberFilter({ defaultValue: { number: 0, comparator: Comparator.GT } })
             },{
               dataField: 'BuyPrice',
               text: 'Buy Price',
-              sort: true
+              sort: true,
+              filter: numberFilter({ defaultValue: { number: 10000, comparator: Comparator.GT } })
             },{
               dataField: 'SellPrice',
               text: 'Sell Price',
-              sort: true
+              sort: true,
+              filter: numberFilter({ defaultValue: { number: 10000, comparator: Comparator.GT } })
             }]
 
     return (
         <div className="Events">
           <h4>{props.client_name}</h4>
           <h4>Trade Data</h4>
-            <Table hover>
-                <thead className="table-header">
-                  <tr>
-                    <th>Date</th>
-                    <th>Trading Member</th>
-                    <th>Client Code</th>
-                    <th>Symbol</th>
-                    <th>ISIN</th>
-                    <th>Share Volume</th>
-                    <th>Buy Price</th>
-                    <th>Sell Price</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {props.data.map((item) =>
-                    (<tr>
-                      <td>{item.Date}</td>
-                      <td>{item.TradingMember}</td>
-                      <td>{item.ClientCode}</td>
-                      <td>{item.OwnsSymbol}</td>
-                      <td>{item.ISIN}</td>
-                      <td>{item.Volume}</td>
-                      <td>{item.BuyPrice}</td>
-                      <td>{item.SellPrice}</td>
+            {/*<Table hover>
+                            <thead className="table-header">
+                              <tr>
+                                <th>Date</th>
+                                <th>Trading Member</th>
+                                <th>Client Code</th>
+                                <th>Symbol</th>
+                                <th>ISIN</th>
+                                <th>Share Volume</th>
+                                <th>Sell Price</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {props.data.map((item) =>
+                                (<tr>
+                                  <td>{item.Date}</td>
+                                  <td>{item.TradingMember}</td>
+                                  <td>{item.ClientCode}</td>
+                                  <td>{item.OwnsSymbol}</td>
+                                  <td>{item.ISIN}</td>
+                                  <td>{item.Volume}</td>
+                                  <td>{item.BuyPrice}</td>
+                                  <td>{item.SellPrice}</td>
 
-                    </tr>)
-                  )}
-                  </tbody>
-            </Table>
-            <BootstrapTable keyField="Date" data={props.data} columns = {columns} />
+                                </tr>)
+                              )}
+                              </tbody>
+                        </Table> */}
+            <BootstrapTable keyField={'Date'}  data={props.data} columns={columns} filter={filterFactory()}   id="table-class" headerClasses="header-class" rowClasses={ "row-class" } />
         </div>
     )
 }
